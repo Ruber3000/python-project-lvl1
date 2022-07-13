9329e07bbf506b5b091b1051e6c1bf0ca9ca4749
Andrey Ruber <nav1978@mail.ru>
2022-07-13 09:05:52 +0300

    try make Stap 5

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+'''
 def main():
     print('Welcome to the Brain Games!')
     from ..cli import welcome_user
     welcome_user()
-
+'''
 
 if __name__ == '__main__':
     main()
