d43edc2e513e0a5eda8313c7a12e77340bc46dac
Andrey Ruber <nav1978@mail.ru>
2022-07-18 12:30:41 +0300

    brain games step7

---
+++ brain_games/games/gcd_code.py
@@ -0,0 +1,27 @@
+import prompt
+import random
+import math
+
+
+def nod_function():
+    name = prompt.string('- May I have your name? \n- ')
+    print(f"- Hello, {name}!")
+    i = 0
+    while i < 3:
+        print("\n- Find the greatest common divisor of given numbers.")
+        number1 = random.randrange(3, 100)
+        number2 = random.randrange(2, 100)
+        answer = math.gcd(number1, number2)
+        print(f'- Question: {number1} {number2}')
+        answer_user = prompt.string('- Your answer: ')
+        if answer_user == str(answer):
+            print('- Correct!')
+            i += 1
+        else:
+            print(f"\n- '{answer_user}' is wrong answer ;(. Correct answer was '{answer}'.")
+            i = 4
+            break
+    if i == 3:
+        print(f'\n- Congratulations, {name}!')
+    else:
+        print(f"- Let's try again, {name}!")
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+print('Welcome to the Brain Games!')
+
+
+def main():
+    from ..games.gcd_code import nod_function
+    nod_function()
+
+
+if __name__ == '__main__':
+    main()
